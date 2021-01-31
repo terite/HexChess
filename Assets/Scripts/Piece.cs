3b2aac4f0e09e961021c9b36765b37d4f2d7c00d
Avea <aveakrause@gmail.com>
2021-01-30 21:11:48 -0800

    Pieces all spawn at awake

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public Team team;
+}
+
+public enum PieceType {
+    King, Queen, KingsRook, QueensRook, KingsKnight, QueensKnight, KingsBishop, QueensBishop, WhiteSquire, GraySquire, BlackSquire, 
+    Pawn1, Pawn2, Pawn3, Pawn4, Pawn5, Pawn6, Pawn7, Pawn8
+}
