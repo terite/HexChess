70e5655f3c28f852faf3d9212c63c472842ce03a
Avea <aveakrause@gmail.com>
2021-02-02 15:42:37 -0800

    added ability to load any board state

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 
 public class BoardManager : SerializedMonoBehaviour
 {
-    public BoardState startingState;
-    public Dictionary<(Team, PieceType), GameObject> prefabs = new Dictionary<(Team, PieceType), GameObject>();
+    public List<BoardState> turnHistory = new List<BoardState>();
+    public Dictionary<(Team, PieceType), GameObject> piecePrefabs = new Dictionary<(Team, PieceType), GameObject>();
+    public Dictionary<(Team, PieceType), GameObject> activePieces = new Dictionary<(Team, PieceType), GameObject>();
     [SerializeField] private HexSpawner boardSpawner;
 
-    private void Awake() => SpawnTeams();
-
-    private void SpawnTeams()
-    {
-        foreach(KeyValuePair<(Team, PieceType), (int, int)> kvp in startingState.piecePositions)
-        {
-            (int row, int col) = kvp.Value;
-            Vector3 hexLoc = boardSpawner.hexes[row][col].transform.position;
-            GameObject newPiece = Instantiate(prefabs[kvp.Key], hexLoc + Vector3.up, Quaternion.identity);
-        }
-    }
+    private void Awake() => SetBoardState(turnHistory[0]);
 
     private void SetBoardState(BoardState newState)
     {
+        foreach(KeyValuePair<(Team, PieceType), (int, int)> pieceAtLocation in newState.piecePositions)
+        {
+            (int row, int col) = pieceAtLocation.Value;
+            Vector3 hexLoc = boardSpawner.hexes[row][col].transform.position;
 
+            // If the piece already exists, move it
+            if(activePieces.ContainsKey(pieceAtLocation.Key))
+            {
+                activePieces[pieceAtLocation.Key].transform.position = hexLoc;
+                continue;
+            }
+
+            // Spawn a new piece at the proper location
+            activePieces.Add(
+                pieceAtLocation.Key, 
+                Instantiate(piecePrefabs[pieceAtLocation.Key], hexLoc + Vector3.up, Quaternion.identity)
+            );
+        }
     }
 }
