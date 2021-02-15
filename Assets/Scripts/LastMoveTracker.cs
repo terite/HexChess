f580bc1bd45945009c789fc435b85265aa56000c
Avea <aveakrause@gmail.com>
2021-02-14 22:48:17 -0800

    Removed an unused class
    also made type of p clearer in a switch.

--- Assets/Scripts/LastMoveTracker.cs
+++ Assets/Scripts/LastMoveTracker.cs
@@ -29,26 +29,21 @@ public class LastMoveTracker : MonoBehaviour
         bool isEven = i.row % 2 == 0;
 
         return i.col switch{
-            0 when !isEven => "A",
-            0 when isEven => "B",
-            1 when !isEven => "C",
-            1 when isEven => "D",
-            2 when !isEven => "E",
-            2 when isEven => "F",
-            3 when !isEven => "G",
-            3 when isEven => "H",
-            4 => "I",
-            _ => ""
+            0 when !isEven => "A", 0 when isEven => "B",
+            1 when !isEven => "C", 1 when isEven => "D",
+            2 when !isEven => "E", 2 when isEven => "F",
+            3 when !isEven => "G", 3 when isEven => "H",
+            4 => "I", _ => ""
         };
     }
     public string GetPieceString(Piece piece) => piece switch {
         Piece.King => "King",
         Piece.Queen => "Queen",
-        var p when (p == Piece.KingsKnight || p == Piece.QueensKnight) => "Knight",
-        var p when (p == Piece.KingsRook || p == Piece.QueensRook) => "Rook",
-        var p when (p == Piece.KingsBishop || p == Piece.QueensBishop) => "Bishop",
-        var p when (p == Piece.WhiteSquire || p == Piece.GraySquire || p == Piece.BlackSquire) => "Squire",
-        var p when (
+        Piece p when (p == Piece.KingsKnight || p == Piece.QueensKnight) => "Knight",
+        Piece p when (p == Piece.KingsRook || p == Piece.QueensRook) => "Rook",
+        Piece p when (p == Piece.KingsBishop || p == Piece.QueensBishop) => "Bishop",
+        Piece p when (p == Piece.WhiteSquire || p == Piece.GraySquire || p == Piece.BlackSquire) => "Squire",
+        Piece p when (
             p == Piece.Pawn1 || p == Piece.Pawn2 ||
             p == Piece.Pawn3 || p == Piece.Pawn4 ||
             p == Piece.Pawn5 || p == Piece.Pawn6 ||
