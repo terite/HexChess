ad5cfa4b4fd8a39d08159b46e46f7d9c2102cbe2
David Stensland <me@terite.com>
2021-04-19 17:35:16 -0700

    Rewrite CheckCompleteMessage to not drop some messages
    When two messages are received within a small time window, they will be
    put in one byte[]. The previous CheckCompleteMessage implementation only
    handled the first message when bytes came in from the network. Now, all
    messages will be handled.
    
    This new implementation also avoids several allocations, and moves
    message parsing code into the message class where it belongs.

--- Assets/Scripts/Message.cs
+++ Assets/Scripts/Message.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 
 public struct Message 
 {
@@ -59,4 +60,27 @@ public struct Message
     }
 
     public static Span<byte> GetSignature() => new byte[5] {1, 2, 3, 4, 5};
+
+    public static (int msgLen, Message message)? ReadMessage(ReadOnlySpan<byte> buffer)
+    {
+        int headerLength = 5 + 2 + 1; 
+        if (buffer.Length < headerLength)
+            return null;
+
+        var signatureSpan = buffer.Slice(0, 5);
+        if (!signatureSpan.SequenceEqual(GetSignature()))
+            throw new ArgumentException("Invalid message signature!");
+
+        var payloadLenSpan = buffer.Slice(5, 2);
+        ushort payloadLength = BitConverter.ToUInt16(payloadLenSpan.ToArray(), 0);
+        var messageType = (MessageType)buffer[7];
+
+        var totalLength = headerLength + payloadLength;
+        if (buffer.Length < totalLength)
+            return null;
+
+        var payload = buffer.Slice(headerLength, payloadLength).ToArray();
+
+        return (totalLength, new Message(messageType, payload));
+    }
 }
