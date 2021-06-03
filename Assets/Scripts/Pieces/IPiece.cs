9729b7c2e4ac655f7fc1f4796142d1317a43ea0c
David Stensland <me@terite.com>
2021-06-03 15:48:14 -0700

    Move Piece to StandaloneRules

--- Assets/Scripts/Pieces/IPiece.cs
+++ Assets/Scripts/Pieces/IPiece.cs
@@ -17,8 +17,3 @@ public interface IPiece
     void Init(Team team, Piece piece, Index startingLocation);
     string GetPieceString();
 }
-
-public enum Piece {
-    King, Queen, KingsRook, QueensRook, KingsKnight, QueensKnight, KingsBishop, QueensBishop, WhiteSquire, GraySquire, BlackSquire, 
-    Pawn1, Pawn2, Pawn3, Pawn4, Pawn5, Pawn6, Pawn7, Pawn8
-}
