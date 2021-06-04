a86a255d69ddbd5fc1b9067f77642d7ede8c3aaa
David Stensland <me@terite.com>
2021-06-04 15:41:22 -0700

    Implement MoveGenerator.GetAllPossibleBishopMoves

--- Assets/Scripts/StandaloneRules/MoveGenerator.cs
+++ Assets/Scripts/StandaloneRules/MoveGenerator.cs
@@ -93,4 +93,77 @@ public static class MoveGenerator
             }
         }
     }
+
+    public static IEnumerable<(Index, MoveType)> GetAllPossibleBishopMoves(Index location, Team team, BoardState boardState, bool includeBlocking = false)
+    {
+        List<(Index, MoveType)> possible = new List<(Index, MoveType)>();
+        int offset = location.row % 2;
+
+        // Top Left
+        for(
+            (int row, int col, int i) = (location.row + 1, location.col - offset, 0);
+            row <= Index.rows && col >= 0;
+            row++, i++
+        ){
+            if(!BishopCanMove(team, boardState, row, col, possible, includeBlocking))
+                break;
+
+            if(i % 2 == offset)
+                col--;
+        }
+        // Top Right
+        for(
+            (int row, int col, int i) = (location.row + 1, location.col + Mathf.Abs(1 - offset), 0);
+            row <= Index.rows && col <= Index.cols;
+            row++, i++
+        ){
+            if(!BishopCanMove(team, boardState, row, col, possible, includeBlocking))
+                break;
+
+            if(i % 2 != offset)
+                col++;
+        }
+        // Bottom Left
+        for(
+            (int row, int col, int i) = (location.row - 1, location.col - offset, 0);
+            row >= 0 && col >= 0;
+            row--, i++
+        ){
+            if(!BishopCanMove(team, boardState, row, col, possible, includeBlocking))
+                break;
+
+            if(i % 2 == offset)
+                col--;
+        }
+        // Bottom Right
+        for(
+            (int row, int col, int i) = (location.row - 1, location.col + Mathf.Abs(1 - offset), 0);
+            row >= 0 && col <= Index.cols;
+            row--, i++
+        ){
+            if(!BishopCanMove(team, boardState, row, col, possible, includeBlocking))
+                break;
+
+            if(i % 2 != offset)
+                col++;
+        }
+
+        return possible;
+    }
+
+    private static bool BishopCanMove(Team team, BoardState boardState, int row, int col, List<(Index, MoveType)> possible, bool includeBlocking = false)
+    {
+        Index index = new Index(row, col);
+        if (!index.IsInBounds)
+            return false;
+
+        if (boardState.TryGetPiece(index, out (Team team, Piece piece) occupier))
+        {
+            if (occupier.team != team || includeBlocking)
+                possible.Add((index, MoveType.Attack));
+            return false;
+        }
+        possible.Add((index, MoveType.Move));
+        return true;
+    }
 }
