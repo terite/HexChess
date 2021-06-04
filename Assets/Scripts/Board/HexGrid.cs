b5b255bdb26632e5fa2b6e0dfa18fe2854c5c3ed
David Stensland <me@terite.com>
2021-06-04 14:56:26 -0700

    Move Index bounds checking from HexGrid to Index

--- Assets/Scripts/Board/HexGrid.cs
+++ Assets/Scripts/Board/HexGrid.cs
@@ -6,10 +6,10 @@ using System;
 
 public struct HexGrid
 {
-    public const int cols = 5;
-    public static int maxRow => rows - 1;
-    public const int rows = 19;
-    public static int maxCol => cols - 1;
+    public const int cols = Index.cols;
+    public const int maxCol = Index.maxCol;
+    public const int rows = Index.rows;
+    public const int maxRow = Index.maxRow;
     public float radius;
     public float height;
     [SerializeField, MinMaxSlider(-64, 64, true)] private Vector2 hexHeightVariance;
@@ -21,12 +21,8 @@ public struct HexGrid
     public float Apothem =>
        (radius.Sqr() - (radius * 0.5f).Sqr()).Sqrt();
 
-    public static bool IsInBounds(int row, int col) {
-        bool cond1 = row * (row - maxRow) <= 0 && col* (col - maxCol) <= 0;
-        bool cond2 = !(cols % 2 != 0 && col == cols - 1 && row % 2 == 0);
-        return cond1 && cond2;
-    }
-    public static bool IsInBounds(Index index) => IsInBounds(index.row, index.col);
+    public static bool IsInBounds(int row, int col) => IsInBounds(new Index(row, col));
+    public static bool IsInBounds(Index index) => index.IsInBounds;
 
     public static bool GetValidIndex(int row, int col, out Index index)
     {
@@ -40,21 +36,6 @@ public struct HexGrid
 
     public static Index? GetNeighborAt(Index source, HexNeighborDirection dir)
     {
-        bool isEven = source.row % 2 == 0;
-        (int row, int col) offsets = dir switch
-        {
-            HexNeighborDirection.Up => (2, 0),
-            HexNeighborDirection.UpRight => isEven ? (1, 1) : (1, 0),
-            HexNeighborDirection.DownRight => isEven ? (-1, 1) : (-1, 0),
-            HexNeighborDirection.Down => (-2, 0),
-            HexNeighborDirection.DownLeft => isEven ? (-1, 0) : (-1, -1),
-            HexNeighborDirection.UpLeft => isEven ? (1, 0) : (1, -1),
-            _ => (-100, -100)
-        };
-
-        Index neighbor = new Index(source.row + offsets.row, source.col + offsets.col);
-        if (IsInBounds(neighbor))
-            return neighbor;
-        return null;
+        return source.GetNeighborAt(dir);
     }
 }
