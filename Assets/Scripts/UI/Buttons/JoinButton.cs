a88d02772fbceeac35b373b32873413504c71b05
Avea <aveakrause@gmail.com>
2021-11-26 17:40:43 -0800

    QoL
    When clicking join, the input field is now automatically selected for the player to type/paste an ip.

--- Assets/Scripts/UI/Buttons/JoinButton.cs
+++ Assets/Scripts/UI/Buttons/JoinButton.cs
@@ -1,11 +1,14 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class JoinButton : TwigglyButton
 {
     [SerializeField] private TextMeshProUGUI titleText;
+    [SerializeField] private TMP_InputField ipInput;
     [SerializeField] private GameObject state1Container;
     [SerializeField] private GameObject state2Container;
+
     private new void Awake() {
         base.Awake();
         base.onClick += Clicked;
@@ -16,5 +19,6 @@ public class JoinButton : TwigglyButton
         titleText.text = "JOIN";
         state2Container.SetActive(true);
         state1Container.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(ipInput.gameObject);
     }
 }
